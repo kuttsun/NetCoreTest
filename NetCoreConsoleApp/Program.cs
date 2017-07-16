ebd1a7255466b6200f53241687869c80a4fe7bfa
kuttsun <tsukushimail+github@gmail.com>
2017-07-17 00:26:58 +0900

    XPathテスト

--- NetCoreConsoleApp/Program.cs
+++ NetCoreConsoleApp/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Text;
-using System.IO;
-using System.Xml;
+
 
 
 namespace NetCoreConsoleApp
@@ -13,44 +12,9 @@ namespace NetCoreConsoleApp
             // 文字化け対策 http://kagasu.hatenablog.com/entry/2016/12/07/004813
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
-            Console.WriteLine("Hello World!");
-            XPathTest();
+            XPath.Test();
         }
 
-        /// <summary>
-        /// XPath のテスト
-        /// </summary>
-        static void XPathTest()
-        {
-            string str = @"
-    <doc> 
-        <head> 
-            <title>タイトル</title> 
-        </head> 
-        <body> 
-            <line>一行目</line> 
-            <line>二行目</line> 
-            <line>三行目</line> 
-        </body> 
-    </doc>";
 
-            XmlDocument doc = new XmlDocument();
-            doc.Load(new StringReader(str));
-
-            XmlNodeList nodeList;
-            XmlNode root = doc.DocumentElement;
-
-            // タイトル表示
-            Console.WriteLine(root.SelectSingleNode("head/title").InnerText);
-
-            // 行選択
-            nodeList = root.SelectNodes("body/line");
-
-            foreach (XmlNode nd in nodeList)
-            {
-                // 行表示
-                Console.WriteLine(nd.InnerText);
-            }
-        }
     }
 }
---
+++ NetCoreConsoleApp/XPath.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+namespace NetCoreConsoleApp
+{
+    class XPath
+    {
+        /// <summary>
+        /// XPath のテスト
+        /// </summary>
+        public static void Test()
+        {
+            string str = @"
+    <doc> 
+        <head> 
+            <title>タイトル</title> 
+        </head> 
+        <body> 
+            <line>一行目</line> 
+            <line>二行目</line> 
+            <line>三行目</line> 
+        </body> 
+    </doc>";
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(new StringReader(str));
+
+            XmlNodeList nodeList;
+            XmlNode root = doc.DocumentElement;
+
+            // タイトル表示
+            Console.WriteLine(root.SelectSingleNode("head/title").InnerText);
+
+            // 行選択
+            nodeList = root.SelectNodes("body/line");
+
+            foreach (XmlNode nd in nodeList)
+            {
+                // 行表示
+                Console.WriteLine(nd.InnerText);
+            }
+        }
+    }
+}
