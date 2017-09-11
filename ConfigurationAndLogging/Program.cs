1893b49cb4497c8e1df12aac2a7581822c95ed19
kuttsun <tsukushimail+github@gmail.com>
2017-09-11 13:13:14 +0900

    自分なりいろいろわかりやすくまとめなおし

--- ConfigurationAndLogging/Program.cs
+++ ConfigurationAndLogging/Program.cs
@@ -8,54 +8,79 @@ using System.IO;
 
 namespace ConfigurationAndLogging
 {
-
     public class Program
     {
         // https://stackoverflow.com/questions/38706959/net-core-console-applicatin-configuration-xml
         static void Main(string[] args)
         {
+            // DI を使った Application クラスを例として挙げる
+
+            // IServiceCollection に対してフレームワークが提供する拡張メソッド を使いながら依存性を定義してゆき、
+            // ActivatorUtilities などを使って依存関係が解決されたインスタンスを取得するのが大まかな流れ
+            // add メソッドでサービスを追加すると、DI として構成される
             IServiceCollection serviceCollection = new ServiceCollection();
 
+            // ConfigureServices で DI の準備を行う
             ConfigureServices(serviceCollection);
 
-            // Application application = new Application(serviceCollection);
+            // var app = new Application(serviceCollection);
             IServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
 
+            // DI サービスコンテナから指定した型のサービスを取得する
             var app = serviceProvider.GetService<Application>();
 
-            // For async
-            Task.Run(() => app.Run()).Wait(); // Exceptions thrown here will be lost! Catch them all at Run()
-                                              // Otherwise use sync as in: app.Run();            
+            // 実行
+            app.Run();
+            Console.ReadKey();
         }
 
         private static void ConfigureServices(IServiceCollection services)
         {
+            // ロギングの設定
             ILoggerFactory loggerFactory = new LoggerFactory()
+                // コンソールに出力する
                 .AddConsole()
+                // Visual Studio のデバッグウィンドウに出力する
                 .AddDebug();
 
-            services.AddSingleton(loggerFactory); // Add first my already configured instance
-            services.AddLogging(); // Allow ILogger<T>
+            // DI サービスコンテナに Singleton ライフサイクルにてオブジェクトを登録する
+            // Singleton ライフサイクルでは Dependency インスタンスを一つ生成し、そのインスタンスをアプリケーションで共有する
+            services.AddSingleton(loggerFactory);
+            // AddLogging メソッドを呼び出すことで ILoggerFactory と ILogger<T> が DI 経由で扱えるようになる
+            services.AddLogging();
 
-            IConfigurationRoot configuration = GetConfiguration();
-            services.AddSingleton<IConfigurationRoot>(configuration);
+            // IConfigurationBuilder で設定を選択
+            // IConfigurationBuilder.Build() で設定情報を確定し、IConfigurationRoot を生成する
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                // 基準となるパスを設定
+                .SetBasePath(Directory.GetCurrentDirectory())
+                // ここでどの設定元を使うか指定
+                // 同じキーが設定されている場合、後にAddしたものが優先される
+                .AddJsonFile($"appsettings.json", optional: true)
+                // ここでは JSON より環境変数を優先している
+                .AddEnvironmentVariables()
+                // 上記の設定を実際に適用して構成読み込み用のオブジェクトを得る
+                .Build();
 
-            // Support typed Options
+            // Logger と同じく DI サービスコンテナに Singleton ライフサイクルにてオブジェクトを登録する
+            services.AddSingleton(configuration);
+
+            // オプションパターンを有効にすることで、構成ファイルに記述した階層構造データを POCO オブジェクトに読み込めるようにする
             services.AddOptions();
+
+            // Configure<T> を使ってオプションを初期化する
+            // IConfigurationRoot から GetSection 及び GetChildren で個々の設定の取り出しができる
+            // ここでは "MyOptions" セクションの内容を MyOptions として登録
             services.Configure<MyOptions>(configuration.GetSection("MyOptions"));
 
-            services.AddTransient<Application>();
-        }
 
-        private static IConfigurationRoot GetConfiguration()
-        {
-            return new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile($"appsettings.json", optional: true)
-                .Build();
+            // Application を DI サービスコンテナに登録する
+            // AddTransient はインジェクション毎にインスタンスが生成される
+            services.AddTransient<Application>();
         }
     }
 
+    // オプションを保持するためのクラス
     public class MyOptions
     {
         public string Name { get; set; }
@@ -63,24 +88,36 @@ namespace ConfigurationAndLogging
 
     public class Application
     {
-        ILogger _logger;
-        MyOptions _settings;
+        ILogger logger;
+        MyOptions settings;
 
+        // コンストラクタの引数として ILogger や IOptions 型の引数を定義すると、.NET Core の DI 機能によりオブジェクトが注入される
+        // (コンストラクタインジェクション)
+        // 設定を取得する際には IOptions<T> を経由して DI から値を受け取る
         public Application(ILogger<Application> logger, IOptions<MyOptions> settings)
         {
-            _logger = logger;
-            _settings = settings.Value;
+            this.logger = logger;
+            // ここで受け取れるオブジェクトは、オブジェクト自体ではなくアクセサオブジェクトであるため、Value プロパティを参照している
+            this.settings = settings.Value;
         }
 
-        public async Task Run()
+        public void Run()
         {
+            logger.LogCritical("Log Critical");
+            logger.LogError("Log Error");
+            logger.LogWarning("Log Warning");
+            logger.LogInformation("Log Information");
+            // 以下の２つはデフォルトでは出力されない
+            logger.LogDebug("Log Debug");
+            logger.LogTrace("Log Trace");
+
             try
             {
-                _logger.LogInformation($"This is a console application for {_settings.Name}");
+                logger.LogInformation($"This is a console application for {settings.Name}");
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex.ToString());
+                logger.LogError(ex.ToString());
             }
         }
     }
