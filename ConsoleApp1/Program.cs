70910bf7e692ee4cf1bac8455ecbad4c8f013d8e
kuttsun <tsukushimail+github@gmail.com>
2017-11-27 13:32:20 +0900

    参照関係のテスト

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // ConsoleApp2 は単独でも実行可能だが、
+            // ConsoleApp1 からもメソッドをコールできる
+            ConsoleApp2.Test.Output();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ConsoleApp2 は ConsoleApp1 から参照されているが、
+            // ConsoleApp2 単独でも実行可能
+
+            Console.WriteLine("ConsoleApp2 Hello World!");
+
+            Test.Output();
+
+            Console.ReadKey();
+        }
+
+        
+    }
+
+    public class Test
+    {
+        // このメソッドは ConsoleApp1 からコールされている
+        public static void Output()
+        {
+            Console.WriteLine("ConsoleApp2");
+        }
+    }
+}
