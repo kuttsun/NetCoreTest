969003756680848676d213c0469da7a9e8c91c90
kuttsun <tsukushimail+github@gmail.com>
2019-06-21 11:29:54 +0900

    InfluxDb 修正

--- InfluxDB/Program.cs
+++ InfluxDB/Program.cs
@@ -12,83 +12,89 @@ namespace InfluxDB
         static async Task Main(string[] args)
         {
             var randam = new Random();
-            var dbName = "TestDb";
 
             var influxDbClient = new InfluxDbClient("http://localhost:8086/", "root", "root", InfluxDbVersion.Latest);
 
             // データベースの新規作成
+            var dbName = "TestDb";
             var response = await influxDbClient.Database.CreateDatabaseAsync(dbName);
 
-            // データの書き込み
-            var point = new Point()
+            // とりあえず適当なデータを１日分書き込む
+            var dataTimeBase = DateTime.Today.AddDays(-1);// データの書き込み開始日時（昨日からにする）
+            var interval = 30;// 30秒ごとにデータを記録する
+            var count = 24 * 60 * 60 / interval;// 1日分の書き込み回数
+            var lastProc = 0;
+            var proc = 0;
+            for (int i = 0; i < count; i++)
             {
-                // 所属する Measurement の名前
-                Name = "reading",
-                Tags = new Dictionary<string, object>()
+                await influxDbClient.Client.WriteAsync(CreateCpuPoint(1, i), dbName);
+                await influxDbClient.Client.WriteAsync(CreateCpuPoint(2, i), dbName);
+                await influxDbClient.Client.WriteAsync(CreateNetworkPoint(1, i), dbName);
+                await influxDbClient.Client.WriteAsync(CreateNetworkPoint(2, i), dbName);
+
+                // 書き込む量が多いので、進捗率を表示する
+                proc = i * 100 / count;
+                if (proc != lastProc)
                 {
-                    { "SensorId", 8 },
-                    { "SerialNumber", "00AF123B" }
-                },
-                Fields = new Dictionary<string, object>()
-                {
-                    { "Open", randam.Next(200, 300) },
-                    { "High", randam.Next(300, 400) },
-                    { "Low", randam.Next(100, 200) },
-                    { "Close", randam.Next(200, 300) }
-                },
-                // タイムスタンプを指定しなかった場合はサーバー側のタイムスタンプで記録される
-                //Timestamp = DateTime.UtcNow
-            };
+                    Console.WriteLine($"{proc}% ({i}/{count})");
+                    lastProc = proc;
+                }
+            }
 
 
+            //// データの読み込み
+            //var queries = new[]
+            //{
+            //    "SELECT * FROM reading WHERE time > now() - 1h",
+            //    "SELECT * FROM reading WHERE time > now() - 2h"
+            //};
+            //var res2 = await influxDbClient.Client.QueryAsync(queries, dbName);
+            ////var series = await influxDbClient.Serie.GetSeriesAsync(dbName);
 
-            await WritePoint("BitFlyer");
-            await WritePoint("BitMex");
-
-
-            // データの読み込み
-            var queries = new[]
-            {
-                "SELECT * FROM reading WHERE time > now() - 1h",
-                "SELECT * FROM reading WHERE time > now() - 2h"
-            };
-            var res2 = await influxDbClient.Client.QueryAsync(queries, dbName);
-            //var series = await influxDbClient.Serie.GetSeriesAsync(dbName);
+            Console.WriteLine("Completed");
+            Console.ReadLine();
 
 
-            Console.WriteLine("Hello World!");
-
-
-            Point CreatePoint(string name, int index)
+            // CPU のセンサー情報を作成
+            Point CreateCpuPoint(int id, int index)
             {
                 return new Point()
                 {
                     // 所属する Measurement の名前
-                    Name = name,
+                    Name = "CPU",
                     Tags = new Dictionary<string, object>()
                         {
-                            { "SensorId", 8 },
-                            { "SerialNumber", "00AF123B" }
+                            { "CpuId", id },
                         },
                     Fields = new Dictionary<string, object>()
                         {
-                            { "Open", randam.Next(200, 300) },
-                            { "High", randam.Next(300, 400) },
-                            { "Low", randam.Next(100, 200) },
-                            { "Close", randam.Next(200, 300) }
+                            { "Used", randam.Next(0, 100) },// CPU 使用率
+                            { "Temperature", randam.Next(10, 50) },// 温度
                         },
                     // タイムスタンプを指定しなかった場合はサーバー側のタイムスタンプで記録される
-                    Timestamp = DateTime.UtcNow.AddSeconds(index),
+                    Timestamp = dataTimeBase.AddSeconds(index * interval),// 時間をずらして記録
                 };
             }
 
-            async Task WritePoint(string name)
+            // ネットワーク使用量を作成
+            Point CreateNetworkPoint(int id, int index)
             {
-                // とりあえず適当な数書き込む
-                for (int i = 0; i < 10; i++)
+                return new Point()
                 {
-                    await influxDbClient.Client.WriteAsync(CreatePoint(name, i), dbName);
-                }
+                    // 所属する Measurement の名前
+                    Name = "Network",
+                    Tags = new Dictionary<string, object>()
+                        {
+                            { "EthernetId", id },
+                        },
+                    Fields = new Dictionary<string, object>()
+                        {
+                            { "Send", randam.Next(0, 10000) },// 送信量
+                            { "Recv", randam.Next(0, 10000) },// 受信量
+                        },
+                    // タイムスタンプを指定しなかった場合はサーバー側のタイムスタンプで記録される
+                    Timestamp = dataTimeBase.AddSeconds(index * interval),// 時間をずらして記録
+                };
             }
         }
     }
